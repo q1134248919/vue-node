5ccc8d4dbb35c8a5a633a555b24f6ca1d718f0a8
全嘉浩 <1134248919@qq.com>
2019-10-28 18:18:57 +0800

    feat: 新增登陆和注册测试例子

--- src/lib/ApiSetting.js
+++ src/lib/ApiSetting.js
@@ -1,15 +1,15 @@
 import http from "../lib/http";
 
-export const getAccount=()=>{
-   return  http.get('/api/login/getAccount')
-}
-export const createAccount=(params)=>{
-	return  http.post('/api/login/createAccount',params)
- }
+export const getAccount = () => {
+  return http.get("/api/login/getAccount");
+};
+export const createAccount = params => {
+  return http.post("/api/login/createAccount", params);
+};
 
- export const checkAccount=(params)=>{
-	return  http.post('/api/login/checkAccount',params)
- }
- export const Loginin=(params)=>{
-	return  http.post('/api/login/loginin',params)
- }
+export const checkAccount = params => {
+  return http.post("/api/login/checkAccount", params);
+};
+export const Loginin = params => {
+  return http.post("/api/login/loginin", params);
+};
--- src/page/Signup.vue
+++ src/page/Signup.vue
@@ -1,55 +1,61 @@
 <!--  -->
 <template>
-  <div class='warpper'>
-
-     <div>
+  <div class="warpper">
+    <div>
       <div class="account">
         账号：
-      <input type="text" v-model='account' >
+        <input type="text" v-model="account" />
       </div>
-      <p>密码：<input type="text" v-model='password'></p>
-      <button @click='login' class="create">登陆</button>
+      <p>
+        密码：
+        <input type="text" v-model="password" />
+      </p>
+      <button @click="login" class="create">登陆</button>
     </div>
   </div>
 </template>
 
 <script>
-import {Loginin} from '../lib/ApiSetting'
+import { Loginin } from "../lib/ApiSetting";
 export default {
-  name:'signup',
-  data () {
+  name: "signup",
+  data() {
     return {
-      account:'',
-      password:'',
+      account: "",
+      password: ""
     };
   },
 
   components: {},
 
-  computed: {
-  },
+  computed: {},
 
-  mounted:function(){},
+  mounted: function() {},
 
   methods: {
-    async login(){
-      if(this.account===''||this.password===''){
+    async login() {
+      if (this.account === "" || this.password === "") {
         return false;
       }
-       const res =  await  Loginin({account:this.account,password:this.password});
-        let {name,time,id}=res.data;
-        let newdata={name,time,id};
-      if(res.data.length>1){
+      const res = await Loginin({
+        account: this.account,
+        password: this.password
+      });
+      let { name, time, id } = res.data;
+      let newdata = { name, time, id };
+      newdata = JSON.stringify(newdata);
+      if (res.data.length >= 1) {
+        localStorage.setItem("loginStatus", true);
+        localStorage.setItem("userInfo", JSON.stringify(newdata));
+        this.$store.dispatch("getUserInfo", newdata);
         this.$store.dispatch("changeLoginStatus");
-        localStorage.setItem("userInfo",newdata);
-      }
-      else{
+        this.$router.push("/");
+      } else {
         alert("账号或者密码错误");
       }
     }
   }
-}
-
+};
 </script>
 <style lang='css' scoped>
 </style>
